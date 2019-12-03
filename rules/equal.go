3db3ee008fb69ae5d91b5102deb878ddfbea76af
Stevenson <smarquez@condorlabs.io>
2019-12-03 10:46:52 -0500

    equal rule

--- rules/equal.go
+++ rules/equal.go
@@ -1,10 +1,41 @@
 package rules
 
-import "github.com/yaien/structure/core"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/yaien/structure/core"
+)
 
 // Equal validate if the field's value is equal to another given field
 func Equal(params []string) (core.ValidateFunc, error) {
+	if len(params) != 1 {
+		return nil, errors.New("equal rule needs to have one param")
+	}
+
 	return func(item *core.Item) error {
-		return nil
+		field := reflect.ValueOf(item.Source).FieldByName(params[0]).Interface()
+		switch item.Value.Kind() {
+		case reflect.String:
+			value := item.Value.String()
+			if value != field.(string) {
+				return fmt.Errorf("should be equal to the '%s' field", params[0])
+			}
+			return nil
+		case reflect.Int:
+			value := int(item.Value.Int())
+			if value != field.(int) {
+				return fmt.Errorf("should be equal to the '%s' field", params[0])
+			}
+			return nil
+		case reflect.Bool:
+			value := item.Value.Bool()
+			if value != field.(bool) {
+				return fmt.Errorf("should be equal to the '%s' field", params[0])
+			}
+			return nil
+		}
+		return fmt.Errorf("unsupported type %v", item.Value.Type())
 	}, nil
 }
--- rules/equal_test.go
+++ rules/equal_test.go
@@ -9,30 +9,30 @@ import (
 
 func TestEqual(t *testing.T) {
 	type structure struct {
-		value        interface{}
-		confirmation interface{}
+		Value        interface{}
+		Confirmation interface{}
 	}
 	testcases := []struct {
-		data   *structure
+		data   structure
 		target interface{}
 		valid  bool
 	}{
-		{data: &structure{"password", "password"}, valid: true},
-		{data: &structure{"password", "nopassword"}, valid: false},
-		{data: &structure{2, 3}, valid: false},
-		{data: &structure{3, 3}, valid: true},
-		{data: &structure{false, true}, valid: false},
-		{data: &structure{true, true}, valid: true},
+		{data: structure{"password", "password"}, valid: true},
+		{data: structure{"password", "nopassword"}, valid: false},
+		{data: structure{2, 3}, valid: false},
+		{data: structure{3, 3}, valid: true},
+		{data: structure{false, true}, valid: false},
+		{data: structure{true, true}, valid: true},
 	}
 
-	validate, err := Equal([]string{"confirmation"})
+	validate, err := Equal([]string{"Confirmation"})
 	if err != nil {
 		t.Fatal(err)
 	}
 	for _, testcase := range testcases {
 		item := &core.Item{
 			Source: testcase.data,
-			Value:  reflect.ValueOf(testcase.data.value),
+			Value:  reflect.ValueOf(testcase.data.Value),
 		}
 		err := validate(item)
 		valid := err == nil
